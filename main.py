2623fdf3485bfad6050dd4c2feb4c0e4262e062e
Parth Maheshwari <138772414+Parth7078@users.noreply.github.com>
2025-05-15 12:02:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main.restapi11 import app 
-
-@pytest.fixture
-def a():
-    return app.test_client()
-
 def test_all_data(a):
     response = a.get('/data')
     assert response.status_code == 200
